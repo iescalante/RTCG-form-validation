dfbc45198cac1194afcd9ecf8d241612fc95f44b
Ivan Escalante <ivan-enrique.escalante-cornejo@ibm.com>
2021-09-27 21:04:13 -0400

    first commit

---
+++ src/components/BasicForm.js
@@ -0,0 +1,105 @@
+import useInput from "../hooks/useInput";
+
+const isNotEmpty = (value) => value.trim() !== "";
+const isEmail = (value) => value.includes("@");
+
+const BasicForm = () => {
+  const {
+    value: firstNameValue,
+    isValid: firstNameIsValid,
+    hasError: firstNameHasError,
+    valueChange: firstNameChangeHandler,
+    valueBlur: firstNameBlurHandler,
+    reset: resetFirstName,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: lastNameValue,
+    isValid: lastNameIsValid,
+    hasError: lastNameHasError,
+    valueChange: lastNameChangeHandler,
+    valueBlur: lastNameBlurHandler,
+    reset: resetLastName,
+  } = useInput(isNotEmpty);
+
+  const {
+    value: emailValue,
+    isValid: emailIsValid,
+    hasError: emailHasError,
+    valueChange: emailChangeHandler,
+    valueBlur: emailBlurHandler,
+    reset: resetEmail,
+  } = useInput(isEmail);
+
+  const firstNameClasses = firstNameHasError
+    ? "form-control invalid"
+    : "form-control";
+  const lastNameClasses = lastNameHasError
+    ? "form-control invalid"
+    : "form-control";
+  const emailClasses = emailHasError ? "form-control invalid" : "form-control";
+
+  let formIsValid = firstNameIsValid && lastNameIsValid && emailIsValid;
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    if (!formIsValid) {
+      return;
+    }
+
+    console.log("submitted");
+    console.log({ firstNameValue, lastNameValue, emailValue });
+
+    resetFirstName();
+    resetLastName();
+    resetEmail();
+  };
+
+  return (
+    <form onSubmit={submitForm}>
+      <div className="control-group">
+        <div className={firstNameClasses}>
+          <label htmlFor="name">First Name</label>
+          <input
+            type="text"
+            id="name"
+            value={firstNameValue}
+            onChange={firstNameChangeHandler}
+            onBlur={firstNameBlurHandler}
+          />
+          {firstNameHasError && (
+            <p className="error-text">Enter a first name</p>
+          )}
+        </div>
+        <div className={lastNameClasses}>
+          <label htmlFor="name">Last Name</label>
+          <input
+            type="text"
+            id="name"
+            value={lastNameValue}
+            onChange={lastNameChangeHandler}
+            onBlur={lastNameBlurHandler}
+          />
+          {lastNameHasError && <p className="error-text">Enter a last name</p>}
+        </div>
+      </div>
+      <div className={emailClasses}>
+        <label htmlFor="name">E-Mail Address</label>
+        <input
+          type="text"
+          id="name"
+          value={emailValue}
+          onChange={emailChangeHandler}
+          onBlur={emailBlurHandler}
+        />
+        {emailHasError && <p className="error-text">Enter a valid email</p>}
+      </div>
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default BasicForm;
---
+++ src/hooks/useInput.js
@@ -0,0 +1,51 @@
+import { useReducer } from "react";
+
+const initialInputState = {
+  value: "",
+  isTouched: false,
+};
+
+const inputStateReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT":
+      return { value: action.value, isTouched: state.isTouched };
+    case "BLUR":
+      return { isTouched: true, value: state.value };
+    case "RESET":
+      return { isTouched: false, value: "" };
+    default:
+      return;
+  }
+};
+
+const useInput = (validateValue) => {
+  const [inputState, dispatch] = useReducer(
+    inputStateReducer,
+    initialInputState
+  );
+
+  const valueIsValid = validateValue(inputState.value);
+  const hasError = !valueIsValid && inputState.isTouched;
+
+  const valueChange = (e) => {
+    dispatch({ type: "INPUT", value: e.target.value });
+  };
+  const valueBlur = (e) => {
+    dispatch({ type: "BLUR" });
+  };
+
+  const reset = () => {
+    dispatch({ type: "RESET" });
+  };
+
+  return {
+    value: inputState.value,
+    hasError,
+    valueChange,
+    valueBlur,
+    isValid: valueIsValid,
+    reset,
+  };
+};
+
+export default useInput;
